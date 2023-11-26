a618f22c0b43feeed8fa51bad6c0808e7e04cead
Mike <mike.s3crypto@gmail.com>
2023-11-26 11:46:33 +0000

    Rectangle Implementation

--- SpreadexWidget/Widgets/Rectangle.cs
+++ SpreadexWidget/Widgets/Rectangle.cs
@@ -1,16 +1,31 @@
 ﻿using System;
 namespace SpreadexWidget.Widgets
 {
-	public class Rectangle : IWidget
-	{
-		public Rectangle()
-		{
-		}
+    public class Rectangle : IWidget
+    {
+        public int X { get; private set; }
+        public int Y { get; private set; }
+        public int Width { get; private set; }
+        public int Height { get; private set; }
+
+        public Rectangle(int x, int y, int width, int height)
+        {
+            if (width <= 0 || height <= 0)
+            {
+                throw new ArgumentException("Width and Height must be positive.");
+            }
+
+            X = x;
+            Y = y;
+            Width = width;
+            Height = height;
+        }
 
         public string Draw()
         {
-            return "";
+            return $"Rectangle ({X},{Y}) width={Width} height={Height}";
         }
     }
+
 }
 
--- WidgetTests/WidgetTests.cs
+++ WidgetTests/WidgetTests.cs
@@ -6,16 +6,73 @@ namespace WidgetTests
     public class RectangleTests
     {
         [TestMethod]
-        public void Draw_WhenCalled_ReturnsEmptyString()
+        public void Constructor_ValidParameters_SetsPropertiesCorrectly()
         {
             // Arrange
-            var rectangle = new Rectangle();
+            int x = 10, y = 20, width = 30, height = 40;
+
+            // Act
+            var rectangle = new Rectangle(x, y, width, height);
+
+            // Assert
+            Assert.AreEqual(x, rectangle.X);
+            Assert.AreEqual(y, rectangle.Y);
+            Assert.AreEqual(width, rectangle.Width);
+            Assert.AreEqual(height, rectangle.Height);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void Constructor_NegativeWidth_ThrowsArgumentException()
+        {
+            // Arrange & Act
+            var rectangle = new Rectangle(10, 20, -30, 40);
+
+            // Assert is handled by ExpectedException
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void Constructor_NegativeHeight_ThrowsArgumentException()
+        {
+            // Arrange & Act
+            var rectangle = new Rectangle(10, 20, 30, -40);
+
+            // Assert is handled by ExpectedException
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void Constructor_ZeroWidth_ThrowsArgumentException()
+        {
+            // Arrange & Act
+            var rectangle = new Rectangle(10, 20, 0, 40);
+
+            // Assert is handled by ExpectedException
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void Constructor_ZeroHeight_ThrowsArgumentException()
+        {
+            // Arrange & Act
+            var rectangle = new Rectangle(10, 20, 30, 0);
+
+            // Assert is handled by ExpectedException
+        }
+
+        [TestMethod]
+        public void Draw_WhenCalled_ReturnsCorrectStringFormat()
+        {
+            // Arrange
+            var rectangle = new Rectangle(10, 20, 30, 40);
+            var expectedOutput = "Rectangle (10,20) width=30 height=40";
 
             // Act
             var result = rectangle.Draw();
 
             // Assert
-            Assert.AreEqual("", result);
+            Assert.AreEqual(expectedOutput, result);
         }
     }
 

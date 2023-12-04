893514503e72f80e1c42869b001fc35129946d2d
Mike <mike.s3crypto@gmail.com>
2023-12-04 15:56:00 +0000

    Use Widget Factory in Main()

--- SpreadexWidget/Program.cs
+++ SpreadexWidget/Program.cs
@@ -10,12 +10,14 @@ namespace SpreadexWidget
             // Create a new canvas
             var canvas = new Canvas();
 
+            var widgetFactory = new WidgetFactory();
+
             // Create widgets
-            var rectangle = new Rectangle(10, 10, 30, 40);
-            var square = new Square(15, 30, 35);
-            var ellipse = new Ellipse(100, 150, 300, 200);
-            var circle = new Circle(1, 1, 300);
-            var textbox = new Textbox(5, 5, 200, 100, "sample text");
+            var rectangle = widgetFactory.CreateRectangle(10, 10, 30, 40);
+            var square = widgetFactory.CreateSquare(15, 30, 35);
+            var ellipse = widgetFactory.CreateEllipse(100, 150, 300, 200);
+            var circle = widgetFactory.CreateCircle(1, 1, 300);
+            var textbox = widgetFactory.CreateTextbox(5, 5, 200, 100, "sample text");
 
             // Add widgets to the canvas
             canvas.AddWidget(rectangle);
